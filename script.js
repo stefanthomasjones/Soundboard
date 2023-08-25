d4c125f0f89f36133c941a3d4e798f8e7a35eef0
Stefan Jones <139777900+stefanthomasjones@users.noreply.github.com>
2023-08-25 15:49:39 +0100

    upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+// Background Colours
+const collection = document.querySelectorAll(".box");
+let colours = ['red','blue','green','yellow','cyan','orange', 'BlueViolet', 'Turquoise', 'Teal', 'BlanchedAlmond', 'DarkBlue','DarkSalmon','DeepPink', 'Indigo','SeaGreen',  ];
+// Adding Sound 
+
+
+// Picking a background colour
+for (let i = 0; i < collection.length; i++) {
+    ramColor =  Math.floor(Math.random()*colours.length);
+    collection[i].style.backgroundColor = colours[ramColor];
+};
+
+// On Click
+
+
+
+window.addEventListener("load", ()=>{
+    const sound = document.querySelectorAll(".sound");
+    const container = document.querySelectorAll(".box");
+
+    container.forEach((box, index)=> {
+        box.addEventListener("click", function(){
+
+            sound[index].currentTime = 0;  // play for ever click
+            sound[index].play();
+        })
+    })
+})
+
+
+// keydown
+
+keys = ['a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v']
+
+const sounds = document.querySelectorAll(".sound");
+document.addEventListener('keydown', e => {
+    const key = e.key
+    const keyIdx = keys.indexOf(key)
+    
+    if (keyIdx > -1) sounds[keyIdx].play()
+    
+})
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+   
